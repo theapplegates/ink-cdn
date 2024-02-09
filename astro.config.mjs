9f6a5c5bc16e5082f08b45fa2aa35884da329088
Paul <paul@theapplegates.com>
2024-02-09 09:31:28 -0500

    new post images

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,42 +5,38 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 import markdoc from "@astrojs/markdoc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import remarkCodeTitles from 'remark-code-titles'
+import remarkCodeTitles from 'remark-code-titles';
 import decapCmsOauth from "astro-decap-cms-oauth";
+import { imageService } from "@unpic/astro/service";  
 
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
+
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  output: 'server',
-  site: 'https://astro-ink.vercel.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  image: {
+    service: imageService(),
+  },
+  output: "server",
+  site: 'https://ink-cdn.paulapplegate.com',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   server: {
     // port: 4321, // The port to run the dev server on.
   },
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: 'css-variables',
+      theme: 'css-variables'
     },
-    remarkPlugins: [
-      remarkCodeTitles
-    ]
+    remarkPlugins: [remarkCodeTitles]
   },
-  integrations: [
-    mdx(), 
-    markdoc(),
-    svelte(), 
-    tailwind({
-      applyBaseStyles: false,
-    }), 
-    sitemap(),
-    decapCmsOauth()
-  ],
+  integrations: [mdx(), markdoc(), svelte(), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), decapCmsOauth()],
   vite: {
     plugins: [],
     resolve: {
@@ -52,5 +48,5 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
       allowNodeBuiltins: true
     }
   },
-  adapter: vercel()
+
 });
